0ac3c002f48a5f61e06ed6dd0af2b7d218da8be6
Andreas <andreas.lofkvist@glnet.se>
2025-02-23 07:33:17 +0100

    ..

--- database/database.go
+++ database/database.go
@@ -12,14 +12,14 @@ var DB *gorm.DB
 func InitDB() {
 	var err error
 	// Connect to SQLite database (you can replace this with any other database driver)
-	DB, err = gorm.Open(sqlite.Open("./database.db"), &gorm.Config{})
+	DB, err = gorm.Open(sqlite.Open("~/database.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
 	DB = DB.Set("gorm:query_option", "WHERE deleted_at IS NULL")
 
-	// Auto migrate the User model
+	// Auto migrate 
 	migrateDb()
 
 	log.Println("Database connected and migrated successfully")
