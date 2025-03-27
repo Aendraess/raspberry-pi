d61b43212e7c50ce84fdbdf031cf57c9e2ddf2d6
Andreas <andreas.lofkvist@glnet.se>
2025-03-27 21:18:06 +0100

    s

--- database/database.go
+++ database/database.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 func InitDB() {
 	var err error
 	// Connect to SQLite database (you can replace this with any other database driver)
-	DB, err = gorm.Open(sqlite.Open("~/database.db"), &gorm.Config{})
+	DB, err = gorm.Open(sqlite.Open("./database.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
